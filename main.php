f6d69324064af2b007d59a1b9dfe6a63a681dcbe
karel tyll <125545316+lilpol@users.noreply.github.com>
2025-01-16 09:35:46 +0100

    Add files via upload

---
+++ main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="center_div">
+        <a href="about.php">ABOUT</a>
+        <a href="main.php">HOME</a>
+        <a href="login.php">LOGIN</a>
+    </div>
